5f50f587b8a286bd86f7054363dbdc9de71ed8be
Cecilia Vanin <ceciliaavanin@gmail.com>
2016-03-27 23:37:41 -0300

    Terceira parte.

--- src/ch/makery/address/view/PersonOverviewController.java
+++ src/ch/makery/address/view/PersonOverviewController.java
@@ -4,8 +4,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+
+import org.controlsfx.dialog.Dialogs;
+
 import ch.makery.address.MainApp;
 import ch.makery.address.model.Person;
+import ch.makery.address.util.DateUtil;
 
 public class PersonOverviewController {
     @FXML
@@ -37,16 +41,52 @@ public class PersonOverviewController {
      */
     public PersonOverviewController() {
     }
-
+    /**
+     * PReenche todos os campos de texto para mostrar detalhes sobre a pessoa.
+     * Se a pessoa especificada for null, todos os campos de texto são limpos.
+     * 
+     * @param person a pessoa ou null
+     */
+    private void showPersonDetails(Person person) {
+    	birthdayLabel.setText(DateUtil.format(person.getBirthday()));
+        }
+    
     /**
      * Inicializa a classe controller. Este método é chamado automaticamente
      *  após o arquivo fxml ter sido carregado.
      */
     @FXML
     private void initialize() {
-        // Inicializa a tablea de pessoa com duas colunas.
-        firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
-        lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+        // Inicializa a tabela de pessoas com duas colunas.
+        firstNameColumn.setCellValueFactory(
+                cellData -> cellData.getValue().firstNameProperty());
+        lastNameColumn.setCellValueFactory(
+                cellData -> cellData.getValue().lastNameProperty());
+
+        // Limpa os detalhes da pessoa.
+        showPersonDetails(null);
+
+        // Detecta mudanças de seleção e mostra os detalhes da pessoa quando houver mudança.
+        personTable.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> showPersonDetails(newValue));
+    }
+    /**
+     * Chamado quando o usuário clica no botão delete.
+     */
+    @FXML
+    private void handleDeletePerson() {
+        int selectedIndex = personTable.getSelectionModel().getSelectedIndex();
+        if (selectedIndex >= 0) {
+            personTable.getItems().remove(selectedIndex);
+        } else {
+            // Nada selecionado.
+            Dialogs.create()
+                .title("Nenhuma seleção")
+                .masthead("Nenhuma Pessoa Selecionada")
+                .message("Por favor, selecione uma pessoa na tabela.")
+                .showWarning();
+        }
+        
     }
 
     /**
@@ -59,5 +99,40 @@ public class PersonOverviewController {
 
         // Adiciona os dados da observable list na tabela
         personTable.setItems(mainApp.getPersonData());
+    }
+    /**
+     * Chamado quando o usuário clica no botão novo. Abre uma janela para editar
+     * detalhes da nova pessoa.
+     */
+    @FXML
+    private void handleNewPerson() {
+        Person tempPerson = new Person();
+        boolean okClicked = mainApp.showPersonEditDialog(tempPerson);
+        if (okClicked) {
+            mainApp.getPersonData().add(tempPerson);
+        }
+    }
+
+    /**
+     * Chamado quando o usuário clica no botão edit. Abre a janela para editar
+     * detalhes da pessoa selecionada.
+     */
+    @FXML
+    private void handleEditPerson() {
+        Person selectedPerson = personTable.getSelectionModel().getSelectedItem();
+        if (selectedPerson != null) {
+            boolean okClicked = mainApp.showPersonEditDialog(selectedPerson);
+            if (okClicked) {
+                showPersonDetails(selectedPerson);
+            }
+
+        } else {
+            // Nada seleciondo.
+            Dialogs.create()
+                .title("Nenhuma seleção")
+                .masthead("Nenhuma Pessoa Selecionada")
+                .message("Por favor, selecione uma pessoa na tabela.")
+                .showWarning();
+        }
     }
 }
