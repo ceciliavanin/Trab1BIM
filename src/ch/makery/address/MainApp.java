73c350b4e07e4ce0c9ac751a0160468eba952417
Cecilia Vanin <ceciliaavanin@gmail.com>
2016-03-29 20:34:11 -0300

    Adicionando um Ícone de Aplicação

--- src/ch/makery/address/MainApp.java
+++ src/ch/makery/address/MainApp.java
@@ -12,7 +12,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
