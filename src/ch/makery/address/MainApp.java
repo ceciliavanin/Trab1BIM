614671050bbb63b7935f993e447e444abc1004f2
Cecilia Vanin <ceciliaavanin@gmail.com>
2016-04-03 14:47:10 -0300

    Definir o Ícone na Scene (Cena)

--- src/ch/makery/address/MainApp.java
+++ src/ch/makery/address/MainApp.java
@@ -10,8 +10,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
@@ -37,6 +39,9 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
+
+        // Set the application icon.
+        this.primaryStage.getIcons().add(new Image("C:/Users/Cecilia Vanin/Pictures/1.png"));
 
         initRootLayout();
 
